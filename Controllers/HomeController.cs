c4c41dd9553f2dab5e4b27463366a2c1f8d5f1ea
Ricky Thang <ricky.writer.dev@gmail.com>
2023-06-17 16:08:17 -0700

    feat - home controller - add contentful client property

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SierraPainters.Models;
+using Contentful.Core;
 
 namespace SierraPainters.Controllers;
 
@@ -8,9 +9,12 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IContentfulClient _client;
+
+    public HomeController(ILogger<HomeController> logger, IContentfulClient client)
     {
         _logger = logger;
+        _client = client;
     }
 
     public IActionResult Index()
