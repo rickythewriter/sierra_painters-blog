1301dd44bac70e31252830b75261b7084874ee66
Ricky Thang <ricky.writer.dev@gmail.com>
2023-06-17 15:53:18 -0700

    new asp.net mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SierraPainters.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
