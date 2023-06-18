3bcc6461574f8cbe71760faab1304eefe178eacd
Ricky Thang <ricky.writer.dev@gmail.com>
2023-06-18 13:16:15 -0700

    feat - blog post model - add slug property

--- Models/BlogPost.cs
+++ Models/BlogPost.cs
@@ -6,5 +6,6 @@ public class BlogPost
 {
     public SystemProperties Sys { get; set; }
     public string Title { get; set; }
+    public string Slug { get; set; }
     public Document Body { get; set; }
 }
